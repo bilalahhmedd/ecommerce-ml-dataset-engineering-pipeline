36212ce1f440138a60d295504e0300fe7c3beed4
bilal ahmed <bilal.ahhmedd@gmail.com>
2026-06-16 12:21:03 +0500

    added attribute analystics module, list unique ids from crawler output module

---
+++ 02_data_processing_layer/list_unique_ids_from_crawler_output.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import os
+
+    """ this script counts list of unique id accross csv files in multiple folder produced by scraper
+    """
+
+ids =[]
+all_csvs=[]
+for root, directories, files in os.walk("./", topdown=False):
+    for name in files:
+        f=(os.path.join(root, name))
+        if f.endswith(("metadata.csv")):
+            all_csvs.append(f)
+# iterate thorugh each csv file and build list of universal keys out of it
+for csv_file in all_csvs:
+    flag=False
+    try:
+        df=pd.read_csv(csv_file)
+        flag=True
+    except:
+        pass
+    if flag:
+        for id in list(df['prod_id']):
+            ids.append(id)
+
+
+print(len(ids))
+print(len(set(ids)))
